8577f17b858888eda4f8fe5e5c73e386eeea4cf3
Justin Prather <justinprather94@gmail.com>
2024-12-01 22:01:52 -0800

    making neotree better

--- lua/custom/plugins/neotree.lua
+++ lua/custom/plugins/neotree.lua
@@ -1,3 +1,8 @@
+local wk = require 'which-key'
+wk.add {
+  { '\\\\', '<cmd>Neotree toggle<cr>', desc = 'Show/hide Neotree', mode = 'n' },
+}
+
 return {
   'nvim-neo-tree/neo-tree.nvim',
   version = '*',
@@ -6,7 +11,10 @@ return {
     'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
     'MunifTanjim/nui.nvim',
   },
+
   config = function()
-    require('neo-tree').setup {}
+    require('neo-tree').setup {
+      close_if_last_window = true,
+    }
   end,
 }
